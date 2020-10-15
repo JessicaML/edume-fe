7d59bb2e6ae5621d1c66bc248038eba98640ad57
Jessica Leach <jessica.leach@futurelearn.com>
2020-10-15 12:54:30 +0100

    Fetch data and log to console
    Fetches data from edume-server and prints to the console.
    
    Also catches an error if edume-server is not running.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchData } from './services/fetchData';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData('http://localhost:3001/t9', setData, setError);
+  }, []);
+
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {console.log(data)}
+        {console.log(error)}
         <a
           className="App-link"
           href="https://reactjs.org"
